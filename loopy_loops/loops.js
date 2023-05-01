909b96293991fab7f5702769554e08a745c4e32a
rahmeesha reynolds <crazym.weird@gmail.com>
2023-05-01 16:55:12 -0400

    "finished loop practice"

--- loopy_loops/loops.js
+++ loopy_loops/loops.js
@@ -3,11 +3,30 @@ for (let i = 0; i < 100; i++) {
     console.log(i);
 }
 
-for (let i = 0; i > 100; i--) {
+for (let i = 99; i > 0; i--) {
     console.log(i);
 }
 
-a = 100,
-b = 45,
-c = 6,
-d = 0
+let even = 0;
+while (even <= 98) {
+    console.log(even);
+    even = even + 2;
+}
+
+let neve = 98;
+while (neve >= 0) {
+    console.log(neve);
+    neve = neve - 2;
+}
+
+let odd = 1;
+while (odd <= 99) {
+    console.log(odd);
+    odd = odd + 2
+}
+
+let boo = 49;
+while (boo <= 72) {
+    console.log(boo);
+    boo = boo + 1
+}
